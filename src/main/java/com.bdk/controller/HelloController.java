d1d094b2844eaaea4468538fdb0f7c98cc0526cb
Sieurobo196 <32062493+sieurobo196@users.noreply.github.com>
2023-03-28 14:25:28 +0700

    Create HelloController.java

---
+++ src/main/java/com.bdk/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.bdk;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    @GetMapping(value = "/hello")
+    public String getHello() {
+        return "Hello world";
+    }
+
+}
